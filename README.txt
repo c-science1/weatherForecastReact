a7ab124ddd67b8724c86367cef76e1243cbc3de1
Sarah Saad <c_science1@hotmail.com>
2017-06-21 09:41:20 +0100

    updated .gitignore

--- README.txt
+++ README.txt
@@ -1,12 +1,6 @@
- In order to run the application, the required modules are needed to be installed via npm.
 
-Run "npm install" to install the modules in the root folder.
+Run "npm install" and then "npm start" to start the application. 
 
-Run "npm start" to start the application. 
-
-Browse to http://localhost:8080/ to see the weather forecast page pulling data from a weather API.
-
-Note: I would have liked to add unit tests in detail to test rendering of each component.
 
 
 
--- src/components/WeatherUI.js
+++ src/components/WeatherUI.js
@@ -7,40 +7,42 @@ var utils = new utilFunc();
 
 export default class WeatherUI extends Component{
 	render(){
-		return <div>
-            <h4>{this.props.city.name}, {this.props.city.country}</h4>
-                {      this.props.list.map(function(item){
-                        {utils.createNewLists(utils.formatDate(item.dt_txt), item)}
-                        })        
-                }
+		return ( 
+                <div>
+                <h4>{this.props.city.name}, {this.props.city.country}</h4>
+                    {      this.props.list.map(function(item){
+                            {utils.createNewLists(utils.formatDate(item.dt_txt), item)}
+                            })        
+                    }
 
-            <ul className="nav nav-tabs" role="tablist" id="myTabs">
-                <li role="presentation" className="active"><a href="#day1" aria-controls="home" role="tab" data-toggle="tab">{utils.day1Name}</a></li>
-                <li role="presentation"><a href="#day2" aria-controls="Day 2" role="tab">{utils.day2Name}</a></li>
-                <li role="presentation"><a href="#day3" aria-controls="Day 3" role="tab">{utils.day3Name}</a></li>
-                <li role="presentation"><a href="#day4" aria-controls="Day 4" role="tab">{utils.day4Name}</a></li>
-                <li role="presentation"><a href="#day5" aria-controls="Day 5" role="tab">{utils.day5Name}</a></li>
-            </ul>
+                <ul className="nav nav-tabs" role="tablist" id="myTabs">
+                    <li role="presentation" className="active"><a href="#day1" aria-controls="home" role="tab" data-toggle="tab">{utils.day1Name}</a></li>
+                    <li role="presentation"><a href="#day2" aria-controls="Day 2" role="tab">{utils.day2Name}</a></li>
+                    <li role="presentation"><a href="#day3" aria-controls="Day 3" role="tab">{utils.day3Name}</a></li>
+                    <li role="presentation"><a href="#day4" aria-controls="Day 4" role="tab">{utils.day4Name}</a></li>
+                    <li role="presentation"><a href="#day5" aria-controls="Day 5" role="tab">{utils.day5Name}</a></li>
+                </ul>
 
-            <div className="tab-content" id="myTabContent">
-            <div role="tabpanel" className="tab-pane fade in active" id="day1">
-                 <WeatherEachDay key="day1" list={utils.day1} />
+                <div className="tab-content" id="myTabContent">
+                    <div role="tabpanel" className="tab-pane fade in active" id="day1">
+                        <WeatherEachDay key="day1" list={utils.day1} />
+                    </div>
+                    <div role="tabpanel" className="tab-pane fade" id="day2">
+                        <WeatherEachDay  key="day2" list={utils.day2} />
+                    </div>
+                    <div role="tabpanel" className="tab-pane fade" id="day3">
+                        <WeatherEachDay  key="day3" list={utils.day3} />
+                    </div>
+                    <div role="tabpanel" className="tab-pane fade" id="day4">
+                        <WeatherEachDay  key="day4" list={utils.day4} />
+                    </div>
+                    <div role="tabpanel" className="tab-pane fade" id="day5">
+                        <WeatherEachDay  key="day5" list={utils.day5} />
+                    </div>      
+                </div>
+
             </div>
-            <div role="tabpanel" className="tab-pane fade" id="day2">
-                <WeatherEachDay  key="day2" list={utils.day2} />
-            </div>
-            <div role="tabpanel" className="tab-pane fade" id="day3">
-                <WeatherEachDay  key="day3" list={utils.day3} />
-            </div>
-            <div role="tabpanel" className="tab-pane fade" id="day4">
-                <WeatherEachDay  key="day4" list={utils.day4} />
-            </div>
-            <div role="tabpanel" className="tab-pane fade" id="day5">
-                <WeatherEachDay  key="day5" list={utils.day5} />
-            </div>
-                    
-        </div>
-        </div>
+        );
 	}
 }
 
